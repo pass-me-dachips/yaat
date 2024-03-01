f03fd434b0f7feec70bacfbb6719427dec865d15
pass-me-dachips <da40au@gmail.com>
2024-03-01 07:45:40 +0100

    retouched cmds.init output display

--- src/cmds/init.js
+++ src/cmds/init.js
@@ -2,7 +2,6 @@ import { cwd } from "node:process";
 import argsParser from "../lib/argsParser.js";
 import formatPath from "../lib/formatPath.js";
 import { writeFile, mkDir } from "../lib/fsWrite.js";
-import chalk from "chalk";
 import { branch_IndexdotYaat, branch_dotyaatEmbed } from "../init/branch.js";
 import {
   tree_installation,
@@ -11,8 +10,7 @@ import {
   tree_dotyaatconstruct,
   tree_dotyaatEmbed,
 } from "../init/tree.js";
-
-const stdout = (data) => process.stdout.write(data);
+import { stdWrite } from "../lib/std.js";
 
 export default function init(args) {
   const parsedArgs = argsParser(args, ["m-", "t-"]);
@@ -20,28 +18,40 @@ export default function init(args) {
   const title = parsedArgs[1] ?? "Yaat Aplication";
 
   if (mode && (mode === "tree" || mode === "branch")) {
-    stdout(chalk.blue(`[-] initializing app...... \n`));
-    stdout(chalk.blue(`[mode] - ${chalk.white(mode)} \n`));
-    stdout(chalk.blue(`[title]  - ${chalk.white(title)} \n\n`));
+    const HEAD = (heading) => [`[${heading}] `, "green", false, false];
+    const BODY = (content) => [`${content}`, "white", true, false];
+
+    stdWrite(...BODY(`[-] initializing app...... \n`));
+    stdWrite(...HEAD("Mode"));
+    stdWrite(...BODY(mode));
+    stdWrite(...HEAD("Title"));
+    stdWrite(...BODY(title));
 
     const path = (base) => formatPath(cwd(), base);
     mkDir(path("docs"));
     process.chdir(path("docs")); //change the working dir to docs
 
+    const COMPLETEOUTPUT = (mode) => [
+      `[done] ${mode} Build completed 🍹🍹.\n`,
+      "green",
+      false,
+      true,
+    ];
+
     if (mode === "tree") {
-      stdout(chalk.blue.bold(`[!] writing (5) files to \`${cwd()}\` \n`));
+      stdWrite(...BODY("\n" + `[!] writing (5) files to \`${cwd()}\` \n`));
       writeFile(path("introduction.yaat"), tree_introduction(title));
       writeFile(path("installation.yaat"), tree_installation());
       writeFile(path("useCases.yaat"), tree_useCases());
       writeFile(path(".yaatconstruct"), tree_dotyaatconstruct());
       writeFile(path(".yaatEmbed"), tree_dotyaatEmbed());
-      stdout(chalk.white(`[done] Build completed 🍹🍹.\n`));
+      stdWrite(...COMPLETEOUTPUT("Tree"));
     } else {
       // branch
-      stdout(chalk.blue.bold(`[!] writing (2) files to \`${cwd()}\` \n`));
+      stdWrite(...BODY("\n" + `[!] writing (2) files to \`${cwd()}\` \n`));
       writeFile(path("index.yaat"), branch_IndexdotYaat(title));
       writeFile(path(".yaatEmbed"), branch_dotyaatEmbed());
-      stdout(chalk.white(`[done] Build completed 🍹🍹.\n`));
+      stdWrite(...COMPLETEOUTPUT("Branch"));
     }
   } else {
     let message = `Invalid argument parsed. expected \`tree\` or \`branch\`, got \`${parsedArgs[0]}\`.`;
