9ccd3ab501f515371cc8a384fb99b89cf1fac0ee
pass-me-dachips <da40au@gmail.com>
2024-03-07 10:32:02 +0100

    added scrolling to tree.tab

---
+++ src/server/public/assets/scripts/utils.js
@@ -0,0 +1,38 @@
+function setThemeName(palleteId, displayName) {
+  const titleBar = document.getElementById("theme-name-location");
+  const rootElement = document.documentElement;
+
+  titleBar.innerHTML = displayName;
+  rootElement.style.setProperty(
+    "--tintB",
+    `var(--utils-theme-pallete-${palleteId}-tint)`
+  );
+  rootElement.style.setProperty(
+    "--toogle-btn-color",
+    `var(--utils-theme-pallete-${palleteId})`
+  );
+}
+
+(() => {
+  const themes = [
+    { palleteId: "default", displayName: "Default" },
+    { palleteId: "mercury", displayName: "Mercury" },
+    { palleteId: "venus", displayName: "Venus" },
+    { palleteId: "earth", displayName: "Earth" },
+    { palleteId: "mars", displayName: "Mars" },
+    { palleteId: "jupiter", displayName: "Jupiter" },
+    { palleteId: "saturn", displayName: "Saturn" },
+    { palleteId: "uranus", displayName: "Uranus" },
+    { palleteId: "neptune", displayName: "Neptune" },
+    { palleteId: "pluto", displayName: "Pluto" },
+  ];
+  const btn = (elem) =>
+    `<button id="utils-theme-pallete-${elem.palleteId}" 
+     onclick="setThemeName('${elem.palleteId}','${elem.displayName}' )"></button>`;
+
+  document.getElementById("theme-name-location").innerHTML =
+    themes[0].displayName;
+
+  const Location = document.getElementById("util-theme-pallet-box");
+  themes.map((elem) => (Location.innerHTML += btn(elem)));
+})();
